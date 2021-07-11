7e4642c6b270ffa8fa4920ab22c4dd69c3dc348b
LexaNoN_Notebook <alexey.2005.odessit@gmail.com>
2021-07-11 10:55:42 +0300

    [ANY] ReWrite ISP and Router code to main loop. Now it's easy to delete, if you don't need it

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from time import sleep
 
 import logging
 
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 file_handler = logging.FileHandler('logfile.log')
 formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
-logger.info("----- Start new loop! -----")
+
 
 points = 0.0  # Counter of ping error's
 errorcheck = False  # Switch for logger
@@ -34,50 +35,39 @@ def pingmine(ip):  # Try to check
 def checkip(ip):  # Sum point
     global points, errorcheckisp, errorcheckrouter
     check = pingmine(ip)
-    if ip == "92.68.1.1":  # Default gateway of my ISP. NewApex (UA). Thanks for stability!
-        if check is True and errorcheckisp is True:
-            logger.warning("ISP reconnected!")
-            errorcheckisp = False
-            return True
-        elif check is False and errorcheckisp is False:
-            logger.warning("ISP lost!")
-            errorcheckisp = True
-            return False
-
-    elif ip == "192.168.0.1":  # Router check
-        if check is True and errorcheckrouter is True:
-            logger.warning("Router reconnected!")
-            errorcheckrouter = False
-            return True
-        elif check is False and errorcheckrouter is False:
-            logger.warning("Router lost!")
-            errorcheckrouter = True
-            return False
-
-    else:
-        if check is True and points >= 1:  # Border from 0
-            points = points - 1
-        elif check is False and points <= 5:  # To 6
-            points = points + 1
+    if check is True and points >= 1:  # Border from 0
+        points = points - 1
+    elif check is False and points <= 5:  # To 6
+        points = points + 1
 
 
-print("Programm has been started!")
-
+logger.info("----- Start new loop! -----")
 while True:
     sleep(0.5)  # Here we can add some ping addresses.
     checkip("8.8.8.8")  # Google
     checkip("1.1.1.1")  # CloudFare
-    print("Error point: " + str(points))
+    # print("Error point: " + str(points))
 
     if points >= 3 and errorcheck is False:
-        print("Internet lost!")
+        # Lost internet
         logger.warning("Internet lost!")
-        if errorcheckisp is True or checkip("92.68.1.1") is False:
-            checkip("192.168.0.1")
-        else:
-            checkip("92.68.1.1")
+        # Check ISP
+        if pingmine("92.68.1.1") is False:
+            if errorcheckisp is True:
+                logger.warning("ISP reconnected!")
+                errorcheckisp = False
+            elif errorcheckisp is False:
+                logger.warning("ISP lost!")
+                errorcheckisp = True
+                # If ISP is lost, check router
+                if pingmine("192.168.0.1") is False:
+                    if errorcheckrouter is True:
+                        logger.warning("Router reconnected!")
+                        errorcheckrouter = False
+                    elif errorcheckrouter is False:
+                        logger.warning("Router lost!")
+                        errorcheckrouter = True
         errorcheck = True
     elif points <= 3 and errorcheck is True:
-        print("Internet resume!")
         logger.warning("Internet resume!")
         errorcheck = False
